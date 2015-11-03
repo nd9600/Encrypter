afa53f055499b9c0bf7362a040ca22acfb131f13
Nathan Douglas <nd9600@gmail.com>
2015-11-03 16:15:00 +0000

    Start of key generation

--- src/onetimepad/OneTimePad.java
+++ src/onetimepad/OneTimePad.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class OneTimePad {
     
+	//Sets up hashMap for converting
 	private static final Map<String, Integer> lettersToNumbersMap;
 	static
     {
@@ -36,28 +37,63 @@ public class OneTimePad {
         lettersToNumbersMap.put("x", 24);
         lettersToNumbersMap.put("y", 25);
         lettersToNumbersMap.put("z", 26);
+        lettersToNumbersMap.put(" ", 27);
+        lettersToNumbersMap.put(".", 28);
+        lettersToNumbersMap.put(",", 29);
     }
+	
+	//Used in modding each character
+	private static final int modValue = lettersToNumbersMap.size();
+	
+	//Returns an encryption key, whether random or user entered
+	private static String getKey(String choice, int lengthOfKey){
+		return "abc";
+	}
 	
 	public static void main(String[] args){
 		System.out.println("lettersToNumbersMap: " + lettersToNumbersMap);
 		
 		Scanner s = new Scanner(System.in);
 		String outputText = "";
+		String key = "";
+		
+		//Gets input text
 		System.out.println("Enter text to convert: ");
 		String inputText = s.nextLine();
-		s.close();
-		
 		System.out.println("");
 		
+		//Converts the input text to a string of numbers, according to the above hashMap
 		for (int i = 0; i < inputText.length(); i++){
 			String character = Character.toString( inputText.charAt(i) );
 			String value = lettersToNumbersMap.get(character).toString();
 			outputText = (outputText + "," + value);
 		}
+		
+		//Removes the first and last "," characters
 		outputText = outputText.substring(1, (outputText.length()) );
 		
 		System.out.println("Converted: ");
 		System.out.println(outputText);
+		
+		//Lets the user pick between having a random encryption key, or entering their own
+		System.out.println("");
+		System.out.println("Options:");
+		System.out.println("#1: Random key");
+		System.out.println("#2: User-entered key");
+		System.out.print("Choice: ");
+		int option = s.nextInt();
+		
+        switch (option) {
+            case 1:  key = getKey("random" , inputText.length());
+                     break;
+            case 2:  key = getKey("user", inputText.length());
+            break;
+        }
+        
+        System.out.println("");
+		System.out.println("Key: " + key);
+        
+        s.close();
 		
 	}
 	
